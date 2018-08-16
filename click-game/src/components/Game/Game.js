a54f971caf9c2e0bc938a1a991c8eed3a0af39ae
conhy <conhysham@cox.net>
2018-08-16 14:22:25 -0700

    Game is functioning

--- click-game/src/components/Game/Game.js
+++ click-game/src/components/Game/Game.js
@@ -9,33 +9,39 @@ class Game extends Component {
         score: 0,
         highScore: 0,
         losses: 0,
-        clicked: false
+        clicked: []
+    }
+
+    startGame = () => {
+        this.setState({ score: 0, dinosaurs: this.state.dinosaurs.sort(() => (0.5 - Math.random())), clicked: [] });
     }
 
     getDinosaur = (id) => {
-
-        if (!this.state.clicked) {
-            this.setState({clicked: true, score: this.state.score +1})
-        }  
-
-        
-
-        this.setState({
-            
-        })
-        alert(id);
+            //console.log(this.state.dinosaurs);
+           // console.log(id);
+            if (this.state.clicked.includes(this.state.dinosaurs[id])) {
+                this.setState({ losses: this.state.losses + 1 });
+                this.startGame();
+            } else {
+                this.setState({ score: this.state.score + 1, dinosaurs: this.state.dinosaurs.sort(() => (0.5 - Math.random())) });
+                this.state.clicked.push(this.state.dinosaurs[id]);
+                if (this.state.score >= this.state.highScore) {
+                    this.setState({ highScore: this.state.score + 1});
+                }
+            }
     }
 
     render() {
         return (
             <div className="container">
                 <div className="jumbotron">
+                    <p>Current score: {this.state.score}</p>
                     <p>High score: {this.state.highScore}</p>
                     <p>Losses: {this.state.losses}</p>
                 </div>
                 <div className="row">
                     {this.state.dinosaurs.map(dinosaur => (
-                        <Card 
+                        <Card
                             name={dinosaur.name}
                             image={dinosaur.image}
                             id={dinosaur.id}
